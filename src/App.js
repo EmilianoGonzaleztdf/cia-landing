25baa7bf0c1752b946b332c382228f92b916e32a
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-07-02 15:30:54 -0300

    aa

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           <Route path="*" element={<P404 />} />
         </Routes>
         <WhatsAppButton/>
+        <RedesSociales/>
         <Footer />
       </Router>
     </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 import "./Footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <section className="redes-sociales">
-        <h2>Síguenos en redes sociales</h2>
-        <div className="social-icons">
-          <a href="https://www.facebook.com/centrointegralautomotor">
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
-          <a href="https://www.instagram.com/centrointegraldelautomotor/">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-        </div>
-      </section>
       <p className="footer-text">
         © 2023 Centro Integral del Automotor. Todos los derechos reservados.
       </p>
